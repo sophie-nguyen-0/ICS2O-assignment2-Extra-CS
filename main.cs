95eab5d6d9c95f5bd9633adb0324ae629046c5bd
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-19 22:26:28 +0000

    indenting

--- main.cs
+++ main.cs
@@ -9,26 +9,26 @@ class Program
 {
     public static void Main(string[] args)
     {
-          int sideA;
-          int sideB;
-          int sideC;
-          int perimeter;
+        int sideA;
+        int sideB;
+        int sideC;
+        int perimeter;
 
-          Console.WriteLine("calculates perimeter of a triangle.");
-          Console.WriteLine("");
+        Console.WriteLine("calculates perimeter of a triangle.");
+        Console.WriteLine("");
 
-          Console.Write("Enter side a (cm): ");
-          sideA = Convert.ToInt32(Console.ReadLine());
-          Console.Write("Enter side b (cm): ");
-          sideB = Convert.ToInt32(Console.ReadLine());
-          Console.Write("Enter side c (cm): ");
-          sideC = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter side a (cm): ");
+        sideA = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter side b (cm): ");
+        sideB = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter side c (cm): ");
+        sideC = Convert.ToInt32(Console.ReadLine());
 
-          perimeter = (sideA + sideB + sideC);
+        perimeter = (sideA + sideB + sideC);
 
-          Console.WriteLine("");
-          Console.WriteLine("perimeter is: " + perimeter + " cm. ");
+        Console.WriteLine("");
+        Console.WriteLine("perimeter is: " + perimeter + " cm. ");
 
-          Console.WriteLine("\nDone.");
+        Console.WriteLine("\nDone.");
   }
 }
